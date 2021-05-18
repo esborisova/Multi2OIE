9ca84a12e79b1ba2e6ba957cebad599403f3ad05
HLasse <lasseh0310@gmail.com>
2021-05-18 14:22:21 +0200

    autoformatting..

--- main.py
+++ main.py
@@ -24,20 +24,24 @@ def main(args):
         arg_clf_dropout=args.arg_clf_dropout,
         pos_emb_dim=args.pos_emb_dim,
         use_lstm=args.use_lstm,
-        device=args.device)
+        device=args.device,
+    )
 
     trn_loader = load_data(
         data_path=args.trn_data_path,
         batch_size=args.batch_size,
         max_len=args.max_len,
-        tokenizer_config=args.bert_config)
+        tokenizer_config=args.bert_config,
+    )
     dev_loaders = [
         load_data(
             data_path=cur_dev_path,
             batch_size=args.dev_batch_size,
             tokenizer_config=args.bert_config,
-            train=False)
-        for cur_dev_path in args.dev_data_path]
+            train=False,
+        )
+        for cur_dev_path in args.dev_data_path
+    ]
     args.total_steps = round(len(trn_loader) * args.epochs)
     args.warmup_steps = round(args.total_steps / 10)
 
@@ -45,27 +49,38 @@ def main(args):
         model=model,
         lr=args.learning_rate,
         total_steps=args.total_steps,
-        warmup_steps=args.warmup_steps)
+        warmup_steps=args.warmup_steps,
+    )
     model.zero_grad()
     summarizer = SummaryManager(args)
     print("\nTraining Starts\n")
 
-    for epoch in tqdm(range(1, args.epochs + 1), desc='epochs'):
+    for epoch in tqdm(range(1, args.epochs + 1), desc="epochs"):
         trn_results = train(
-            args, epoch, model, trn_loader, dev_loaders,
-            summarizer, optimizer, scheduler)
+            args,
+            epoch,
+            model,
+            trn_loader,
+            dev_loaders,
+            summarizer,
+            optimizer,
+            scheduler,
+        )
 
         # extraction on devset
         dev_iter = zip(args.dev_data_path, args.dev_gold_path, dev_loaders)
         dev_results = list()
         total_sum = 0
         for dev_input, dev_gold, dev_loader in dev_iter:
-            dev_name = dev_input.split('/')[-1].replace('.pkl', '')
-            output_path = os.path.join(args.save_path, f'epoch{epoch}_dev/end_epoch/{dev_name}')
+            dev_name = dev_input.split("/")[-1].replace(".pkl", "")
+            output_path = os.path.join(
+                args.save_path, f"epoch{epoch}_dev/end_epoch/{dev_name}"
+            )
             extract(args, model, dev_loader, output_path)
             dev_result = do_eval(output_path, dev_gold)
-            utils.print_results(f"EPOCH{epoch} EVAL",
-                                dev_result, ["F1  ", "PREC", "REC ", "AUC "])
+            utils.print_results(
+                f"EPOCH{epoch} EVAL", dev_result, ["F1  ", "PREC", "REC ", "AUC "]
+            )
             total_sum += dev_result[0] + dev_result[-1]
             dev_result.append(dev_result[0] + dev_result[-1])
             dev_results += dev_result
@@ -75,38 +90,53 @@ def main(args):
     print("\nTraining Ended\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # settings
-    parser.add_argument('--seed', type=int, default=1)
-    parser.add_argument('--save_path', default='./results')
-    parser.add_argument('--bert_config', default='bert-base-cased', help='or bert-base-multilingual-cased')
-    parser.add_argument('--trn_data_path', default='./datasets/openie4_train.pkl')
-    parser.add_argument('--dev_data_path', nargs='+', default=['./datasets/oie2016_dev.pkl', './datasets/carb_dev.pkl'])
-    parser.add_argument('--dev_gold_path', nargs='+', default=['./evaluate/OIE2016_dev.txt', './carb/CaRB_dev.tsv'])
-    parser.add_argument('--max_len', type=int, default=64)
-    parser.add_argument('--device', default='cuda:0')
-    parser.add_argument('--visible_device', default="0")
-    parser.add_argument('--summary_step', type=int, default=100)
-    parser.add_argument('--use_lstm', nargs='?', const=True, default=False, type=utils.str2bool)
-    parser.add_argument('--binary', nargs='?', const=True, default=False, type=utils.str2bool)
+    parser.add_argument("--seed", type=int, default=1)
+    parser.add_argument("--save_path", default="./results")
+    parser.add_argument(
+        "--bert_config",
+        default="bert-base-cased",
+        help="or bert-base-multilingual-cased",
+    )
+    parser.add_argument("--trn_data_path", default="./datasets/openie4_train.pkl")
+    parser.add_argument(
+        "--dev_data_path",
+        nargs="+",
+        default=["./datasets/oie2016_dev.pkl", "./datasets/carb_dev.pkl"],
+    )
+    parser.add_argument(
+        "--dev_gold_path",
+        nargs="+",
+        default=["./evaluate/OIE2016_dev.txt", "./carb/CaRB_dev.tsv"],
+    )
+    parser.add_argument("--max_len", type=int, default=64)
+    parser.add_argument("--device", default="cuda:0")
+    parser.add_argument("--visible_device", default="0")
+    parser.add_argument("--summary_step", type=int, default=100)
+    parser.add_argument(
+        "--use_lstm", nargs="?", const=True, default=False, type=utils.str2bool
+    )
+    parser.add_argument(
+        "--binary", nargs="?", const=True, default=False, type=utils.str2bool
+    )
 
     # hyper-parameters
-    parser.add_argument('--epochs', type=int, default=1)
-    parser.add_argument('--lstm_dropout', type=float, default=0.)
-    parser.add_argument('--mh_dropout', type=float, default=0.2)
-    parser.add_argument('--pred_clf_dropout', type=float, default=0.)
-    parser.add_argument('--arg_clf_dropout', type=float, default=0.2)
-    parser.add_argument('--batch_size', type=int, default=128)
-    parser.add_argument('--dev_batch_size', type=int, default=32)
-    parser.add_argument('--learning_rate', type=float, default=3e-5)
-    parser.add_argument('--n_arg_heads', type=int, default=8)
-    parser.add_argument('--n_arg_layers', type=int, default=4)
-    parser.add_argument('--pos_emb_dim', type=int, default=64)
+    parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument("--lstm_dropout", type=float, default=0.0)
+    parser.add_argument("--mh_dropout", type=float, default=0.2)
+    parser.add_argument("--pred_clf_dropout", type=float, default=0.0)
+    parser.add_argument("--arg_clf_dropout", type=float, default=0.2)
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--dev_batch_size", type=int, default=32)
+    parser.add_argument("--learning_rate", type=float, default=3e-5)
+    parser.add_argument("--n_arg_heads", type=int, default=8)
+    parser.add_argument("--n_arg_layers", type=int, default=4)
+    parser.add_argument("--pos_emb_dim", type=int, default=64)
     main_args = parser.parse_args()
 
     os.environ["CUDA_VISIBLE_DEVICES"] = main_args.visible_device
     main_args = utils.clean_config(main_args)
     main(main_args)
-
--- test.py
+++ test.py
@@ -15,32 +15,34 @@ from carb.tabReader import TabReader
 
 def get_performance(output_path, gold_path):
     auc, precision, recall, f1 = [None for _ in range(4)]
-    if 'evaluate' in gold_path:
+    if "evaluate" in gold_path:
         matching_func = Matcher.lexicalMatch
-        error_fn = os.path.join(output_path, 'error_idxs.txt')
+        error_fn = os.path.join(output_path, "error_idxs.txt")
 
         evaluator = Benchmark(gold_path)
         reader = GeneralReader()
-        reader.read(os.path.join(output_path, 'extraction.txt'))
+        reader.read(os.path.join(output_path, "extraction.txt"))
 
         (precision, recall, f1), auc = evaluator.compare(
             predicted=reader.oie,
             matchingFunc=matching_func,
-            output_fn=os.path.join(output_path, 'pr_curve.txt'),
-            error_file=error_fn)
-    elif 'carb' in gold_path:
+            output_fn=os.path.join(output_path, "pr_curve.txt"),
+            error_file=error_fn,
+        )
+    elif "carb" in gold_path:
         matching_func = CarbMatcher.binary_linient_tuple_match
-        error_fn = os.path.join(output_path, 'error_idxs.txt')
+        error_fn = os.path.join(output_path, "error_idxs.txt")
 
         evaluator = CarbBenchmark(gold_path)
         reader = TabReader()
-        reader.read(os.path.join(output_path, 'extraction.txt'))
+        reader.read(os.path.join(output_path, "extraction.txt"))
 
         auc, (precision, recall, f1) = evaluator.compare(
             predicted=reader.oie,
             matchingFunc=matching_func,
-            output_fn=os.path.join(output_path, 'pr_curve.txt'),
-            error_file=error_fn)
+            output_fn=os.path.join(output_path, "pr_curve.txt"),
+            error_file=error_fn,
+        )
     return auc, precision, recall, f1
 
 
@@ -59,7 +61,8 @@ def main(args):
         n_arg_layers=args.n_arg_layers,
         pos_emb_dim=args.pos_emb_dim,
         use_lstm=args.use_lstm,
-        device=args.device)
+        device=args.device,
+    )
     model.load_state_dict(torch.load(args.model_path))
     model.zero_grad()
     model.eval()
@@ -68,7 +71,8 @@ def main(args):
         data_path=args.test_data_path,
         batch_size=args.batch_size,
         tokenizer_config=args.bert_config,
-        train=False)
+        train=False,
+    )
     start = time.time()
     extract(args, model, loader, args.save_path)
     print("TIME: ", time.time() - start)
@@ -76,28 +80,31 @@ def main(args):
     utils.print_results("TEST RESULT", test_results, ["F1  ", "PREC", "REC ", "AUC "])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--model_path', default='./results/model.bin')
-    parser.add_argument('--save_path', default='./results/carb_test')
-    parser.add_argument('--bert_config', default='bert-base-cased')
-    parser.add_argument('--test_data_path', default='./datasets/carb_test.pkl')
-    parser.add_argument('--test_gold_path', default='./carb/CaRB_test.tsv')
-    parser.add_argument('--device', default='cuda:0')
-    parser.add_argument('--visible_device', default="0")
-    parser.add_argument('--batch_size', type=int, default=1)
-    parser.add_argument('--pos_emb_dim', type=int, default=64)
-    parser.add_argument('--n_arg_heads', type=int, default=8)
-    parser.add_argument('--n_arg_layers', type=int, default=4)
-    parser.add_argument('--use_lstm', nargs='?', const=True, default=False, type=utils.str2bool)
-    parser.add_argument('--binary', nargs='?', const=True, default=False, type=utils.str2bool)
+    parser.add_argument("--model_path", default="./results/model.bin")
+    parser.add_argument("--save_path", default="./results/carb_test")
+    parser.add_argument("--bert_config", default="bert-base-multilingual-cased")
+    parser.add_argument("--test_data_path", default="./datasets/carb_test.pkl")
+    parser.add_argument("--test_gold_path", default="./carb/CaRB_test.tsv")
+    parser.add_argument("--device", default="cuda:0")
+    parser.add_argument("--visible_device", default="0")
+    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--pos_emb_dim", type=int, default=64)
+    parser.add_argument("--n_arg_heads", type=int, default=8)
+    parser.add_argument("--n_arg_layers", type=int, default=4)
+    parser.add_argument(
+        "--use_lstm", nargs="?", const=True, default=False, type=utils.str2bool
+    )
+    parser.add_argument(
+        "--binary", nargs="?", const=True, default=False, type=utils.str2bool
+    )
     main_args = parser.parse_args()
     os.environ["CUDA_VISIBLE_DEVICES"] = main_args.visible_device
 
     main_args.pred_n_labels = 3
     main_args.arg_n_labels = 9
-    device = torch.device(main_args.device if torch.cuda.is_available() else 'cpu')
+    device = torch.device(main_args.device if torch.cuda.is_available() else "cpu")
     main_args.device = device
     main(main_args)
-
